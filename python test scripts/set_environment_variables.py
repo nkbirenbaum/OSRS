29bb7924815bf93ce81e3d97756df26d8046855e
Nathan Birenbaum <nkbirenbaum@gmail.com>
2023-05-04 14:39:20 -0500

    set env var script - mouse position/click flags

--- python test scripts/set_environment_variables.py
+++ python test scripts/set_environment_variables.py
@@ -2,5 +2,7 @@ import os
 from dotenv import set_key
 
 dotenv_path = os.getcwd() + "\.env"
-set_key(dotenv_path, 'RUNELITE_WINDOW_X', str(123))
-set_key(dotenv_path, 'RUNELITE_WINDOW_Y', str(789))
+# set_key(dotenv_path, 'RUNELITE_WINDOW_X', str(123))
+# set_key(dotenv_path, 'RUNELITE_WINDOW_Y', str(789))
+set_key(dotenv_path, 'FLAG_DEBUG_MOUSE_POSITION', str(1))
+set_key(dotenv_path, 'FLAG_DEBUG_MOUSE_CLICK', str(1))
