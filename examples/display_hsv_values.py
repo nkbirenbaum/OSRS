28f242a73e793b1cc2fd111a75f285be84a1b1c5
Nathan Birenbaum <nkbirenbaum@gmail.com>
2023-05-08 10:10:59 -0500

    display hsv example

---
+++ examples/display_hsv_values.py
@@ -0,0 +1,16 @@
+import os
+import cv2 
+import numpy as np
+from PIL import Image
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r"C:/Program Files/Tesseract-OCR/tesseract.exe"
+
+
+image_file = os.path.join(os.getcwd(), 'examples', 'test images', 'yellow_box.png')
+img_pil = Image.open(image_file)
+img_cv = cv2.cvtColor(np.array(img_pil), cv2.COLOR_BGR2RGB)
+print(img_cv)
+cv2.imshow("Yellow bar", img_cv)
+cv2.waitKey(0)
+
