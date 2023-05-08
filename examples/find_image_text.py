c28a935ee2aa7a9e055918ff5ce9bd732e3156b6
Nathan Birenbaum <nkbirenbaum@gmail.com>
2023-05-07 23:23:16 -0500

    tesseract working after tesseract_cmd path defined

--- examples/find_image_text.py
+++ examples/find_image_text.py
@@ -2,9 +2,11 @@ import os
 import cv2 
 import pytesseract
 
+pytesseract.pytesseract.tesseract_cmd = r"C:/Program Files/Tesseract-OCR/tesseract.exe"
+
 image_path = os.path.join(os.getcwd(), 'images', 'icons', 'runelite.png')
 img = cv2.imread(image_path)
 
 # Adding custom options - see https://nanonets.com/blog/ocr-with-tesseract/
-custom_config = r'--oem 3 --psm 6'
-pytesseract.image_to_string(img, config=custom_config)
+custom_config = r'--oem 3 --psm 7'
+print(pytesseract.image_to_string(img, config=custom_config))
